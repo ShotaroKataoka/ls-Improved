f0eddc9a4e6c9c7ba7550d9d9fa26b59d3db47d4
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-06-23 18:39:46 +0900

    [fix] directory structure.

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 if [ -f $HOME/.zshrc ]; then
-  echo export PATH=\$PATH:`pwd` >> $HOME/.zshrc
+  echo export PATH=\$PATH:`pwd`/bin >> $HOME/.zshrc
   echo add path to `pwd` to .zhshrc
-  source $HOME/.zshrc
 fi
 if [ -f $HOME/.bashrc ]; then
-  echo export PATH=\$PATH:`pwd` >> $HOME/.bashrc
+  echo export PATH=\$PATH:`pwd`/bin >> $HOME/.bashrc
   echo add path to `pwd` to .bashrc
-  source $HOME/.bashrc
 fi
