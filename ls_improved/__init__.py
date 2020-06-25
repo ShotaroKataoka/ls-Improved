2f1d172223f01162cc6f48f3867eea3412b03aaa
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-06-25 14:26:05 +0900

    [mod] version0.2.4.bata0

--- ls_improved/__init__.py
+++ ls_improved/__init__.py
@@ -1,5 +1,5 @@
 __copyright__    = 'Copyright (C) 2020 Shotaro Kataoka'
-__version__      = '0.2.3.beta0'
+__version__      = '0.2.4.beta0'
 __license__      = 'MIT'
 __author__       = 'Shotaro Kataoka'
 __author_email__ = 'shotaro.kata@gmail.com'
