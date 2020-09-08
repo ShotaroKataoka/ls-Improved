2078c3081dd3749b679b962cef29ff20b544fe2b
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2020-09-09 08:29:53 +0900

    mkdiri file description

--- ls_improved/mkdiri.py
+++ ls_improved/mkdiri.py
@@ -2,11 +2,12 @@ import os
 import argparse
 
 from .lsi_text import Text
+from .lsi_itemloader import LsiItemLoader
 from .config import Config
 
 
 class Mkdiri():
-    def __init__(self, dir, description, is_add_mode=False):
+    def __init__(self, dir, description, is_add_mode=False, is_file_mode=False):
         """
         Make directory with description file.
 
@@ -22,8 +23,11 @@ class Mkdiri():
         self.dir = dir
         self.description = description
         self.is_add_mode = is_add_mode
+        self.is_file_mode = is_file_mode
+        config = Config()
 
-        self.desc_name = '.description.lsi'
+        self.desc_name = config.description_name
+        self.file_desc_name = config.file_description_name
 
     def _write_description(self, desc_path, description):
         """
@@ -40,6 +44,29 @@ class Mkdiri():
         with open(desc_path, 'w') as f:
             f.write(description)
 
+    def _write_file_description(self, file_path, description_dict):
+        """
+        Make file description.
+
+        Parameters
+        ----------
+        file_path : String
+            file description path
+        description_dict : Dict
+            description Dict(LsiItemLoader.read_file_description)
+        """
+        keys_list = sorted(list(description_dict.keys()))
+        description = ''
+        for keys in keys_list:
+            desc = description_dict[keys]
+            for i in range(len(desc)):
+                if desc[len(desc)-1-i]!='\n':
+                    break
+            desc = desc[:len(desc)-i]
+            description += '\\/'+keys+'\n'+desc+'\n\n'
+        with open(file_path, 'w') as f:
+            f.write(description)
+
     def _mkdiri(self, dir_path, description):
         """
         Make directory and make description file.
@@ -51,11 +78,40 @@ class Mkdiri():
         description : String
             description text
         """
-        if not os.path.exists(dir_path):
+        dir_path = dir_path+'/' if dir_path[-1] != '/' else dir_path
+        if os.path.exists(dir_path):
+            if self.is_add_mode:
+                self._write_description(dir_path+self.desc_name, description)
+            else:
+                self._raise_file_exists_error(self.dir_path)
+        else:
             os.mkdir(dir_path)
-        self._write_description(dir_path+self.desc_name, description)
+            self._write_description(dir_path+self.desc_name, description)
 
-    def _raise_file_exists_error(self, dir_path, description):
+    def _mkfilei(self, file_path, description):
+        """
+        Make file description.
+
+        Parameters
+        ----------
+        file_path : String
+            File Path
+        description : String
+            description text
+        """
+        split_path = file_path.split('/')
+        if len(split_path)==1:
+            base_path = './'
+        else:
+            base_path = '/'.join(split_path[:-1]) + '/'
+        base_name = split_path[-1]
+        _, description_dict = LsiItemLoader().read_file_description(base_path)
+
+        description = description.replace('\\n', '\n')
+        description_dict[base_name] = description
+        self._write_file_description(base_path+self.file_desc_name, description_dict)
+
+    def _raise_file_exists_error(self, dir_path):
         """
         If file exists, it will be called.
         If not add_mode, Raise error.
@@ -64,15 +120,10 @@ class Mkdiri():
         ----------
         dir_path : String
             Directory Path
-        description : String
-            description text
         """
-        if self.is_add_mode:
-            self._write_description(dir_path+self.desc_name, description)
-        else:
-            print('error: "' + dir_path[:-1] + '" already exists.')
-            print('if you want to add a description, use "-a" option please.')
-            exit()
+        print('error: "' + dir_path[:-1] + '" already exists.')
+        print('if you want to add a description, use "-a" option please.')
+        exit()
 
     def _raise_path_not_found_error(self):
         """
@@ -93,7 +144,10 @@ class Mkdiri():
         Run all functions.
         """
         try:
-            self._mkdiri(self.dir, self.description)
+            if self.is_file_mode:
+                self._mkfilei(self.dir, self.description)
+            else:
+                self._mkdiri(self.dir, self.description)
         except OSError as e:
             e = str(e)
             if 'File exists' in e:
@@ -110,6 +164,7 @@ def main():
     parser.add_argument('dir', type=str, metavar='DirectoryPath',nargs='?', default='', help='Directory which you want to make.')
     parser.add_argument('description', type=str, metavar='STRING' ,nargs='?', default='', help='Description text (default:No description.)')
     parser.add_argument('-a', '--add', action='store_true', help='Add description if directory already exists. (default:False)')
+    parser.add_argument('-f', '--file', action='store_true', help='make file description. (default:False)')
     parser.add_argument('--ANSI-MODE', action='store_true', help='SET ANSI-ESCAPE-SEQUENCE-MODE.')
     parser.add_argument('--LSI-MODE', action='store_true', help='SET LSI-ESCAPE-SEQUENCE-MODE.')
     parser.add_argument('-e', '--SHOW-MODE', action='store_true', help='SET LSI-ESCAPE-MODE.')
@@ -154,7 +209,6 @@ def main():
     dir = args.dir
     if dir == '':
         exit()
-    dir = dir+'/' if dir[-1] != '/' else dir
     description = args.description
     if escape_sequence=='ANSI':
         description = Text(description, ';desc;')
@@ -164,8 +218,9 @@ def main():
             description = description[:-1]
         description = config.get_color('end').join(description)
     is_add_mode = args.add
+    is_file_mode = args.file
 
-    mkdiri = Mkdiri(dir, description, is_add_mode)
+    mkdiri = Mkdiri(dir, description, is_add_mode, is_file_mode)
     mkdiri.run()
 
 if __name__ == '__main__':
