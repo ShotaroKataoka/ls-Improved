3cb5811f465e2c32c16b47b4c5ba63e46b40407d
oba18 <m.oba.0426@gmail.com>
2020-09-16 18:14:19 +0900

    [fix] 🍺

--- ls_improved/lsi_visual.py
+++ ls_improved/lsi_visual.py
@@ -77,7 +77,7 @@ class LsiVisualTransforms():
         if place==2:
             if 'description' in item.keys():
                 item['description'].text = item['description'].text.replace('│', ' ')
-            return '📁', item
+            return '\n🍺 ', item
 
 
     def _concat_item(self, item, place):
