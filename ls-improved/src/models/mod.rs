edee5c14615e05fe3cfe1f1f69f84b4e3c33c191
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2021-08-15 10:16:23 +0900

    👍 impl LsiPath Ordering

--- ls-improved/src/models/mod.rs
+++ ls-improved/src/models/mod.rs
@@ -44,18 +44,33 @@ impl LsiPath {
 
 impl Ord for LsiPath {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.file_name().cmp(&other.file_name())
+        let (name1, name2) = add_prefix_number_to_name_for_ordering(self, other);
+        name1.cmp(&name2)
     }
 }
 
 impl PartialOrd for LsiPath {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.file_name().cmp(other.file_name()))
+        let (name1, name2) = add_prefix_number_to_name_for_ordering(self, other);
+        Some(name1.cmp(&name2))
     }
 }
 
 impl PartialEq for LsiPath {
     fn eq(&self, other: &Self) -> bool {
-        self.file_name() == other.file_name()
+        let (name1, name2) = add_prefix_number_to_name_for_ordering(self, other);
+        name1 == name2
     }
+}
+
+fn add_prefix_number_to_name_for_ordering(name1: &LsiPath, name2: &LsiPath) -> (String, String) {
+        let name1 = match name1 {
+            LsiPath::Dir{..} => "0".to_string() + name1.file_name(),
+            LsiPath::File{..} => "1".to_string() + name1.file_name(),
+        };
+        let name2 = match name2 {
+            LsiPath::Dir{..} => "0".to_string() + name2.file_name(),
+            LsiPath::File{..} => "1".to_string() + name2.file_name(),
+        };
+        (name1, name2)
 }
