f71617f3b6f88fa0b9d22af74e77ce77bbc87481
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2021-09-09 12:26:13 +0900

    ♻️ delete python

--- ls_improved-python/__init__.py
+++
@@ -1,7 +0,0 @@
-__copyright__    = 'Copyright (C) 2020 Shotaro Kataoka'
-__version__      = '0.5.1'
-__license__      = 'MIT'
-__author__       = 'Shotaro Kataoka'
-__author_email__ = 'shotaro.kata@gmail.com'
-__url__          = 'https://github.com/ShotaroKataoka/ls-Improved'
-
